fc710841d86554f065301ce0753357d40915d28e
zwojak <kuba.zwolski11@gmail.com>
2021-05-03 22:45:30 +0200

    full project

---
+++ WpfApp3/WpfApp3/DialogWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Logika interakcji dla klasy DialogWindow.xaml
+    /// </summary>
+    public partial class DialogWindow : Window
+    {
+        private string path;
+        private string name;
+        private bool correct;
+        public DialogWindow(string path)
+        {
+            InitializeComponent();
+            this.path = path;
+            correct = false;
+        }
+
+        private void OkClick(object sender, RoutedEventArgs e)
+        {
+            if((bool)fileRButton.IsChecked && !Regex.IsMatch(fileName.Text, "^[a-zA-Z0-9_~-]{1,8}\\.(txt|php|html)$"))
+            {
+                System.Windows.MessageBox.Show("Wrong name!", "Alert", MessageBoxButton.OK, MessageBoxImage.Information);
+                Close();
+            }
+            if(!(bool)fileRButton.IsChecked && !(bool)directoryRButton.IsChecked)
+            {
+                System.Windows.MessageBox.Show("Choose the type of the file!", "Alert", MessageBoxButton.OK, MessageBoxImage.Information);
+                Close();
+            }
+            name = fileName.Text;
+            path += "\\" + name;
+            if ((bool)fileRButton.IsChecked)
+            {
+                File.Create(path);
+            }
+            else
+            {
+                Directory.CreateDirectory(path);
+            }
+            correct = true;
+            Close();
+        }
+
+        public bool IsFile()
+        {
+            if ((bool)fileRButton.IsChecked) return true;
+            else return false;
+        }
+
+        public bool IsCorrect()
+        {
+            if (correct) return true;
+            else return false;
+        }
+
+
+        public string GetPath()
+        {
+            return path;
+        }
+
+        private void CancelClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
---
+++ WpfApp3/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.IO;
+using System.Windows.Forms;
+using MenuItem = System.Windows.Controls.MenuItem;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Open(object sender, RoutedEventArgs e)
+        {
+            var dlg = new FolderBrowserDialog()
+            {
+                Description = "Select directory to open"
+            };
+            DialogResult result = dlg.ShowDialog();
+            if(result == System.Windows.Forms.DialogResult.OK)
+            {
+                treeView.Items.Clear();
+                DirectoryInfo dir = new DirectoryInfo(dlg.SelectedPath);
+                var root = InsertDir(dir);
+                treeView.Items.Add(root);
+            }
+        }
+
+        private TreeViewItem InsertDir(DirectoryInfo dir)
+        {
+            var root = new TreeViewItem
+            {
+                Header = dir.Name,
+                Tag = dir.FullName
+            };
+            root.ContextMenu = new System.Windows.Controls.ContextMenu();
+            var menuItem1 = new MenuItem { Header = "Create" };
+            menuItem1.Click += new RoutedEventHandler(MenuItemCreateClick);
+            root.ContextMenu.Items.Add(menuItem1);
+            foreach (DirectoryInfo subdir in dir.GetDirectories())
+            {
+                root.Items.Add(InsertDir(subdir));
+            }
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                root.Items.Add(InsertFile(file));
+            }
+            return root;
+        }
+
+        private TreeViewItem InsertFile(FileInfo file)
+        {
+            var item = new TreeViewItem
+            {
+                Header = file.Name,
+                Tag = file.FullName
+            };
+            item.ContextMenu = new System.Windows.Controls.ContextMenu();
+            var menuItem1 = new MenuItem { Header = "Open" };
+            var menuItem2 = new MenuItem { Header = "Delete" };
+            menuItem1.Click += new RoutedEventHandler(MenuItemOpenClick);
+            menuItem2.Click += new RoutedEventHandler(MenuItemDeleteClick);
+            item.ContextMenu.Items.Add(menuItem1);
+            item.ContextMenu.Items.Add(menuItem2);
+            return item;
+        }
+
+        private void MenuItemOpenClick(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = (TreeViewItem)treeView.SelectedItem;
+            string content = File.ReadAllText((string)item.Tag);
+            scrollViewer.Content = new TextBlock() { Text = content };
+        }
+
+        private void MenuItemCreateClick(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = (TreeViewItem)treeView.SelectedItem;
+            string path = (string)item.Tag;
+            DialogWindow dialog = new DialogWindow(path);
+            dialog.ShowDialog();
+            if (dialog.IsCorrect())
+            {
+                if (dialog.IsFile())
+                {
+                    FileInfo file = new FileInfo(dialog.GetPath());
+                    item.Items.Add(InsertFile(file));
+                }
+                else
+                {
+                    DirectoryInfo dir = new DirectoryInfo(dialog.GetPath());
+                    item.Items.Add(InsertDir(dir));
+                }
+            }
+        }
+
+        private void MenuItemDeleteClick(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = (TreeViewItem)treeView.SelectedItem;
+            File.Delete((string)item.Tag);
+            TreeViewItem parent = (TreeViewItem)item.Parent;
+            parent.Items.Remove(item);
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
